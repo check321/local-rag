ea00e3de3b12537bc088ca2112ac200e3b00435e
fyang <check321@aliyun.com>
2024-11-25 01:23:37 +0800

    feat: embedding data to local vectore db.

--- local_rag/rag_demo.py
+++ local_rag/rag_demo.py
@@ -1,9 +1,11 @@
-from llama_index.core import SimpleDirectoryReader
-from llama_index.core import VectorStoreIndex
+from llama_index.core import VectorStoreIndex,SimpleDirectoryReader,StorageContext
 from dotenv import load_dotenv
 import os
 from llama_index.embeddings.dashscope import DashScopeEmbedding, DashScopeTextEmbeddingType,DashScopeTextEmbeddingModels
 from llama_index.llms.dashscope import DashScope, DashScopeGenerationModels
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
 
 load_dotenv()
 
@@ -11,20 +13,24 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 api_key = os.getenv("DASHSCOPE_API_KEY")
 dashscope_llm = DashScope(model_name=DashScopeGenerationModels.QWEN_MAX,api_key=api_key)
 
-documents = SimpleDirectoryReader("./assets").load_data()
-# index = VectorStoreIndex.from_documents(documents)
+# documents = SimpleDirectoryReader("./assets").load_data()
+
+db = chromadb.PersistentClient("./chroma_db")
+chroma_collection = db.get_or_create_collection("journey_to_west")
+
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 embedder = DashScopeEmbedding(
     model_name=DashScopeTextEmbeddingModels.TEXT_EMBEDDING_V2,
     text_type=DashScopeTextEmbeddingType.TEXT_TYPE_DOCUMENT,
     api_key=api_key
 )
-
-index = VectorStoreIndex.from_documents(documents, embed_model=embedder)
+index = VectorStoreIndex.from_vector_store(vector_store,storage_context=storage_context,embed_model=embedder)
 
 query_engine = index.as_query_engine(llm=dashscope_llm)
 
-response = query_engine.query("八戒向妇人要求把女儿嫁给他时，妇人对他说了什么？")
+response = query_engine.query("八戒的人物性格是怎样的？有哪些例子？")
 
 print(response)
 
